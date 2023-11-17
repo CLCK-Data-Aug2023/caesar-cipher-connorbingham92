c369d01b8840ea8e1269c9602ef06e2ee57ebd0a
Connor Bingham <142693389+connorbingham92@users.noreply.github.com>
2023-11-17 08:03:49 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,15 +1,12 @@
-cipher = {}
-for i in range(97,123):
-    cipher[chr(i)] = chr((i+5) % 123 + 97*((i+5)//123))
-
-output = ""
-sentence = input("Please enter a sentence: ")
-for letter in sentence:
-    
-    if letter in cipher.keys():
-        output += cipher[letter]
-    else:
-        output += letter
-
-    print(output)
 # add your code here
+sentence = input("please enter a sentence:") 
+sentence = sentence.lower()
+
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
+output = ""
+
+for letter in sentence: 
+    for i in range(0,len(alphabet)): 
+        if letter == alphabet[i]: output = output + (alphabet[(i+5)%26]) 
+        elif letter not in alphabet: output = output + letter 
+    print(output)
