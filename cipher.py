17243b6f3b0fcd5bf135d787457c3295c9dd1855
Connor Bingham <142693389+connorbingham92@users.noreply.github.com>
2023-11-17 07:49:27 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,15 @@
+cipher = {}
+for i in range(97,123):
+    cipher[chr(i)] = chr((i+5) % 123 + 97*((i+5)//123))
+
+output = ""
+sentence = input("Please enter a sentence: ")
+for letter in sentence:
+    
+    if letter in cipher.keys():
+        output += cipher[letter]
+    else:
+        output += letter
+
+    print(output)
 # add your code here
